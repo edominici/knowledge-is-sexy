55366ce60459f91dba2d936fb05b3c272567de1f
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-04-14 23:54:00 -0500

    html files set up, javascript files and API keys obtained for algolia

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+$(document).ready(function () {
+    console.log("hello");
+    
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBLXtC4nDv-yxymc0nI2uc3ugTUi4PvUco",
@@ -7,12 +10,47 @@
     storageBucket: "knowledgeispower-560e2.appspot.com",
     messagingSenderId: "349167488894"
   };
-  firebase.initializeApp(config);
+  
+    firebase.initializeApp(config);
 
     // Get a reference to the database service
-    var db = firebase.database();
+    var dataRef = firebase.database();
 
-    var client = algoliasearch('ALGOLIA_APP_ID', 'ALGOLIA_API_KEY');
+    var client = algoliasearch('58946AMDV8', '331c73d68fc3d1064f3f7e1bc34b5348');
     var index1 = client.initIndex('questions_db');
     // var index2 = client.initIndex('tags');
- 
+
+    $("#submitButton").on("click", function (event) {
+        
+        
+            event.preventDefault();
+        
+            var questionSearch = $("#questionBox").val().trim();
+       
+            var newEntry = {
+              search: questionSearch,
+              dataAdded: firebase.database.ServerValue.TIMESTAMP
+            }
+
+            console.log(newEntry);
+        
+            dataRef.ref().push(newEntry);
+        
+            //Logs everything to console
+            console.log(newEntry.search);
+
+    dataRef.ref().on("child_added", function (childSnapshot) {
+    
+         console.log(childSnapshot.val());
+                
+        var questionSearch = childSnapshot.val().search;
+
+        console.log(questionSearch);
+
+        $("#card-question").append(questionSearch);
+
+    });
+
+    });
+
+});
