312db3537a5773c7bbe8bc94f48f723bd2254aa5
Michael Ingram <mpingram92@gmail.com>
2018-05-30 12:51:29 -0500

    Set up react-router
    * Move landing page code from App.tsx to own module
    * Rewrite App.tsx to handle routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import * as React from 'react';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import logo from './shared/images/kis-logo.svg';
+import { Landing } from "./landing";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className='App'>
-
-        <header className='App-header'>
-          <div className='App-logo-container'>
-            <div className='App-title'>Knowledge Is&nbsp;<span className='App-title-accent'>Sexy.</span></div>
-            <img src={logo} className='App-logo' alt='logo' />
-          </div>
-        </header>
-
-        <div className='content'>
-
-          <h2 className='content-text'><span className='content-text-accent'>Ask</span>&nbsp;any sex or sexual health questions here.</h2>
-          <div className='search-bar-container'>
-            <input type='search' className='search-bar' placeholder='#AskQuestions'/>
-            <button className='search-bar-submit-button' onClick={this.handleSearchSubmit}>Ask</button>
-          </div>
-          <div className='divider'>
-            <div className='divider-line'/>
-            <div className='divider-text'>
-              or
-            </div>
-          </div>
-          <h2 className='content-text'><span className='content-text-accent'>Browse</span>&nbsp;questions that other people have asked.</h2>
-
-        </div>
-      </div>
-    );
-  }
-
-  private handleSearchSubmit = (ev: React.MouseEvent<HTMLButtonElement>) => {
-    // implement
-  }
-}
-
-export default App;
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Landing} />
+    </BrowserRouter>
+  );
+};
---
+++ src/landing.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './landing.css';
+
+import logo from './shared/images/kis-logo.svg';
+
+export class Landing extends React.Component {
+  public render() {
+    return (
+      <div className='landing'>
+        <header className='landing-header'>
+          <div className='landing-logo-container'>
+            <div className='landing-title'>Knowledge Is&nbsp;<span className='landing-title-accent'>Sexy.</span></div>
+            <img src={logo} className='landing-logo' alt='logo' />
+          </div>
+        </header>
+
+        <div className='content'>
+
+          <h2 className='content-text'><span className='content-text-accent'>Ask</span>&nbsp;any sex or sexual health questions here.</h2>
+          <div className='search-bar-container'>
+            <input type='search' className='search-bar' placeholder='#AskQuestions'/>
+            <button className='search-bar-submit-button' onClick={this.handleSearchSubmit}>Ask</button>
+          </div>
+          <div className='divider'>
+            <div className='divider-line'/>
+            <div className='divider-text'>
+              or
+            </div>
+          </div>
+          <h2 className='content-text'><span className='content-text-accent'>Browse</span>&nbsp;questions that other people have asked.</h2>
+
+        </div>
+      </div>
+    );
+  }
+
+  private handleSearchSubmit = (ev: React.MouseEvent<HTMLButtonElement>) => {
+    // implement
+  }
+}
