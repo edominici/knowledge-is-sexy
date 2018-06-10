7b92f762d1b6d9f1e3f404d1ce86124490a87036
Michael Ingram <mpingram92@gmail.com>
2018-06-10 10:20:29 -0500

    First-pass style question-browse page

--- src/question-browse.tsx
+++ src/question-browse.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import { SpeechBubble } from 'src/shared/components';
 
+import './question-browse.css';
+
 export const QuestionBrowse: React.SFC<any> = (props) => {
 
   const handleSearchClick: React.MouseEventHandler<HTMLButtonElement> = (ev) => {
@@ -12,41 +14,49 @@ export const QuestionBrowse: React.SFC<any> = (props) => {
     <div className='page'>
       <div className='search-bar-header'>
           <div className='search-bar-container'>
-            <input type='search' className='search-bar' placeholder='#AskQuestions'/>
+            <input value='My question is this question. There are others like it but this question is mine' type='search' className='search-bar'/>
             <button className='search-bar-submit-button' onClick={handleSearchClick}>Ask</button>
           </div>
       </div>
-      <div className='question-list'>
-        <button className='question-button'>
-          <SpeechBubble
-            type='question'
-            text='Question text one'
-          />
-        </button>
-        <button className='question-button'>
-          <SpeechBubble
-            type='question'
-            text='Question text one'
-          />
-        </button>
-        <button className='question-button'>
-          <SpeechBubble
-            type='question'
-            text='Question text one'
-          />
-        </button>
-        <button className='question-button'>
-          <SpeechBubble
-            type='question'
-            text='Question text one'
-          />
-        </button>
-        <button className='question-button'>
-          <SpeechBubble
-            type='question'
-            text='Question text one'
-          />
-        </button>
+      <div className='question-list-scroll-container'>
+        <div className='question-list'>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+          <button className='question-button'>
+            <SpeechBubble
+              type='question'
+              text='Question text one'
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
