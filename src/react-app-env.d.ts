374b7f9a96a514df587065129800aa522b32a55f
Michael Ingram <mpingram92@gmail.com>
2018-11-19 13:10:41 -0600

    [WIP] Write data access singleton class
    * DONE sketch out implementations of methods to return popular questions
    and questions in categories
    * DONE load questions from google sheets 'backend' on load
      ** add new dependency papa parse, to parse google sheets csv file
    * TODO implement getQuestionsBySearchString()
    * TODO test

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+declare module 'nuka-carousel';
+declare module 'papaparse';
--- src/shared/data-access.ts
+++ src/shared/data-access.ts
@@ -1,11 +1,47 @@
 import { Question } from '../../src/shared/types';
+import { parse } from 'papaparse';
 
+const GOOGLE_SHEETS_QUESTIONS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSZ3Ag8K_d4V3rzBf9pXQ5J9GInmj9VCeNknjuV_S9sO-yqZOzCg1gbQt3UHdthqXOL24v0Fw4fPrFy/pub?output=csv';
+interface RawGoogleSheetsQuestion {
+  id: string
+  topic: string
+  question: string
+  tags: string
+  answer: string
+}
+const toQuestion = (q: RawGoogleSheetsQuestion): Question => {
+  return {
+    id: q.id,
+    question: q.question,
+    answer: q.answer,
+    tags: q.tags.split(',').map( tag => tag.trim() ),
+    categories: q.topic.split(',').map( category => category.trim() )
+  }
+}
+
+/**
+ * DataAccess is a singleton class that contains methods for accessing 
+ * the app's question data.
+ */
 export class DataAccess {
 
   public static getInstance() {
     if (!DataAccess.instance) {
+      // create the singleton instance
       DataAccess.instance = new DataAccess();
-      // initiate web request 
+
+      // TODO handle network errors here, use repeated download attempts + falloff
+      // initiate loading google sheets data as csv
+      parse(GOOGLE_SHEETS_QUESTIONS_URL, {
+        download: true,
+        header: true,
+        complete: (res: any) => {
+          const rawQuestions: RawGoogleSheetsQuestion[] = res.data;
+          const questions: Question[] = rawQuestions.map( toQuestion );
+          DataAccess.questions = questions;
+        }
+      });
+
     }
     return DataAccess.instance;
   }
@@ -17,7 +53,7 @@ export class DataAccess {
       if (DataAccess.questions === undefined) {
         reject(DataAccess.QuestionsNotLoadedError)
       }
-      // FIXME this implementation only returns the first x questions
+      // FIXME this is a mock implementation that returns a random set of questions
       resolve(DataAccess.questions.slice(0, count));
     });
   }
